9ea5f1b328c7970964f3488102914b45589de393
Nattawut Sudachom <nusnattawut9@gmail.com>
2024-07-15 16:27:51 +0700

    new1

--- Program.cs
+++ Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Vertical}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
